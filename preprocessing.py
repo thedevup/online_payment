a0181e06d9d4b388541a0f4328ecc45df925376b
Nour Eddine Aarab <nrd.future.engineer@gmail.com>
2023-09-13 19:42:47 +0200

    little changes

--- preprocessing.py
+++ preprocessing.py
@@ -12,6 +12,9 @@ df = pd.read_csv('../fin_data.csv')
 # Exploring the fin_data
 print(df.head)
 
+# Check the shape of the dataset
+print(df.shape)
+
 # Show the comulmnsand data types
 print(df.info())
 
@@ -27,10 +30,13 @@ df = df.join(df2[['type_CASH_IN', 'type_CASH_OUT', 'type_DEBIT', 'type_PAYMENT',
 # Drop the columns we are not interested in
 df.drop(['nameOrig', 'nameDest', 'type'], axis=1, inplace=True)
 
+# The shape of the dataset after feature engineering and dropping the unecessary features
+print(df.shape)
+
 # Correlation matrix
 print(df.corr())
 
-# scalling
+# scalling?
 
 
 # Loadthe dependent and undependent varialbles
@@ -38,7 +44,7 @@ X = df.drop('isFraud',axis=1)
 Y = df['isFraud']
 
 # Splitting the data into training and testing sets
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.10,random_state=1)
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.30,random_state=1)
 
 # Create the KNN model
 knn = KNeighborsClassifier(n_neighbors=6)
@@ -56,7 +62,13 @@ Y_pred_nb = nb.fit(X_train, Y_train).predict(X_test)
 print(Y_pred_nb)
 
 # Calculate the accyracy for knn
-print(classification_report(Y_test,Y_pred_knn))
+# print(classification_report(Y_test,Y_pred_knn))
 
-# Calculate the accyracy for nb
-print(classification_report(Y_test,Y_pred_nb))
+# knn accuracy score
+print("knn score: ", accuracy_score(Y_test,Y_pred_knn))
+
+# Calculate the accyracy for NB
+# print(classification_report(Y_test,Y_pred_nb))
+
+# NB accuracy score
+print("knn score: ", accuracy_score(Y_test,Y_pred_nb))
